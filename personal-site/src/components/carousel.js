9ad631f845f4828703a93ac259ff2ed6d8d76d1c
Benjamin Sullivan <bensully513@gmail.com>
2018-07-24 22:15:20 -0600

    third carousel option

--- personal-site/src/components/carousel.js
+++ personal-site/src/components/carousel.js
@@ -60,61 +60,185 @@
 
 // export default ControlledCarousel
 
-import React, { Component } from 'react';
-import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Container } from 'mdbreact';
+// import React, { Component } from 'react';
+// import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Container } from 'mdbreact';
 
-class CarouselPage extends Component {
+// class CarouselPage extends Component {
+//   render(){
+//     return(
+//       <Container>
+//         <h4 className="mt-5 mb-2">Basic example</h4>
+//         <Carousel
+//           activeItem={1}
+//           length={4}
+//           showControls={true}
+//           showIndicators={false}
+//           className="z-depth-1">
+//           <CarouselInner>
+//             <CarouselItem itemId="1">
+//               <View>
+//                 <img className="d-block w-100" src="/skilyft.png" alt="First slide" />
+//                 <Mask overlay="black-light"></Mask>
+//               </View>
+//               <CarouselCaption>
+//                 <h3 className="h3-responsive">Light mask</h3>
+//                 <p>First text</p>
+//               </CarouselCaption>
+//             </CarouselItem>
+//             <CarouselItem itemId="2">
+//               <View>
+//                 <img className="d-block w-100" src="/skilyft.png" alt="Second slide" />
+//                 <Mask overlay="black-strong"></Mask>
+//               </View>
+//               <CarouselCaption>
+//                 <h3 className="h3-responsive">Strong mask</h3>
+//                 <p>Second text</p>
+//               </CarouselCaption>
+//             </CarouselItem>
+//             <CarouselItem itemId="3">
+//               <View>
+//                 <img className="d-block w-100" src="/skilyft.png" alt="Third slide" />
+//                 <Mask overlay="black-slight"></Mask>
+//               </View>
+//               <CarouselCaption>
+//                 <h3 className="h3-responsive">Slight mask</h3>
+//                 <p>Third text</p>
+//               </CarouselCaption>
+//             </CarouselItem>
+//             <CarouselItem itemId="4">
+//               <View>
+//                 <img className="d-block w-100" src="/skilyft.png" alt="Mattonit's item" />
+//                 <Mask overlay="black-light"></Mask>
+//               </View>
+//               <CarouselCaption>
+//                 <h3 className="h3-responsive">Sopot Beach</h3>
+//                 <p>Taken june 21th by @mattonit</p>
+//               </CarouselCaption>
+//             </CarouselItem>
+//           </CarouselInner>
+//         </Carousel>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default CarouselPage;
+
+import React, { Component } from 'react';
+import { Carousel, CarouselInner, CarouselItem, Container, Row, Col, Card, CardImage, CardBody, CardTitle, CardText, Button } from 'mdbreact';
+
+class MultiCarouselPage extends Component {
+
   render(){
     return(
       <Container>
-        <h4 className="mt-5 mb-2">Basic example</h4>
+        <h4 className="mt-5 mb-2">Multi-item Carousel</h4>
         <Carousel
           activeItem={1}
-          length={4}
+          length={3}
+          slide={true}
           showControls={true}
-          showIndicators={false}
-          className="z-depth-1">
+          showIndicators={true}
+          multiItem >
           <CarouselInner>
-            <CarouselItem itemId="1">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" alt="First slide" />
-                <Mask overlay="black-light"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Light mask</h3>
-                <p>First text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="2">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg" alt="Second slide" />
-                <Mask overlay="black-strong"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Strong mask</h3>
-                <p>Second text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="3">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg" alt="Third slide" />
-                <Mask overlay="black-slight"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Slight mask</h3>
-                <p>Third text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="4">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20%28143%29.jpg" alt="Mattonit's item" />
-                <Mask overlay="black-light"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Sopot Beach</h3>
-                <p>Taken june 21th by @mattonit</p>
-              </CarouselCaption>
-            </CarouselItem>
+            <Row>
+              <CarouselItem itemId="1">
+                <Col md="4">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="/skilyft.png" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="/skilyft.png" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="/skilyft.png" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </CarouselItem>
+              <CarouselItem itemId="2">
+                <Col md="4">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="/skilyft.png" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="/skilyft.png" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(48).jpg" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </CarouselItem>
+              <CarouselItem itemId="3">
+                <Col md="4">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(53).jpg" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(45).jpg" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col md="4" className="clearfix d-none d-md-block">
+                  <Card className="mb-2">
+                  <CardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(41).jpg" />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                      <Button color="primary">Button</Button>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </CarouselItem>
+            </Row>
           </CarouselInner>
         </Carousel>
       </Container>
@@ -122,4 +246,4 @@ class CarouselPage extends Component {
   }
 }
 
-export default CarouselPage;
+export default MultiCarouselPage;
